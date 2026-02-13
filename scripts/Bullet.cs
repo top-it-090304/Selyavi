ef779d126aefddb482aa1ee9977621a1c938831f
Max <plof0676@gmail.com>
2026-02-13 09:41:41 +0600

    setter for bulletSpeed

--- scripts/Bullet.cs
+++ scripts/Bullet.cs
@@ -3,9 +3,20 @@ using System;
 
 public class Bullet : Area2D
 {
+	#region private fields
 	private int _bulletSpeed = 7;
 	private Vector2 _velocity = Vector2.Zero;
 	private AudioStreamPlayer _plasmaSound;
+	#endregion
+	
+	public int BulletSpeed{
+		get => _bulletSpeed;
+		set {
+			if(value > 0 && value <= 30){
+				_bulletSpeed = value;
+			}
+		}
+	}
 
 
 
