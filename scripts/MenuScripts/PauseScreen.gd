2831e8d28b991c904b69a00db048ccc37719a842
Max <plof0676@gmail.com>
2026-04-03 16:05:29 +0600

    redesigned pause screen

--- scripts/MenuScripts/PauseScreen.gd
+++ scripts/MenuScripts/PauseScreen.gd
@@ -1,13 +1,27 @@
 extends CanvasLayer
 
+@onready var sidebar = $Sidebar
+@onready var dim_overlay = $DimOverlay
+
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
-	# Прячем UI игрока при входе в паузу
 	_toggle_player_ui(false)
 
+	# Анимация появления
+	sidebar.position.x = -300
+	dim_overlay.modulate.a = 0
+	var tween = create_tween().set_pause_mode(Tween.TWEEN_PAUSE_PROCESS).set_parallel(true)
+	tween.tween_property(sidebar, "position:x", 0, 0.4).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween.tween_property(dim_overlay, "modulate:a", 1.0, 0.4)
+
 func _on_ReturnToGameButton_pressed():
+	# Анимация исчезновения
+	var tween = create_tween().set_pause_mode(Tween.TWEEN_PAUSE_PROCESS).set_parallel(true)
+	tween.tween_property(sidebar, "position:x", -300, 0.3).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN)
+	tween.tween_property(dim_overlay, "modulate:a", 0.0, 0.3)
+
+	await tween.finished
 	get_tree().paused = false
-	# Возвращаем UI игрока перед выходом
 	_toggle_player_ui(true)
 	queue_free()
 
@@ -16,7 +30,6 @@ func _on_RestartButton_pressed():
 	get_tree().reload_current_scene()
 
 func _on_ReturnToSettingsButton_pressed():
-	# Сохраняем информацию, что мы перешли из игры
 	if GameManager != null:
 		GameManager.set_meta("from_scene", get_tree().current_scene.scene_file_path)
 	get_tree().paused = false
@@ -27,9 +40,8 @@ func _on_ReturnToMenuButton_pressed():
 	get_tree().change_scene_to_file("res://scenes/MenuScenes/Menu.tscn")
 
 func _toggle_player_ui(is_visible: bool):
-	var tree = get_tree()
-	# Скрываем все CanvasLayer в сцене, кроме текущего (экрана паузы)
-	_recursive_toggle_ui(tree.root, is_visible)
+	# Рекурсивно скрываем все CanvasLayer, кроме этого экрана паузы
+	_recursive_toggle_ui(get_tree().root, is_visible)
 
 func _recursive_toggle_ui(node: Node, is_visible: bool):
 	if node is CanvasLayer and node != self:
