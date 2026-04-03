8ff35a2dfc6803a1e4489be7676e2bc094100145
Max <plof0676@gmail.com>
2026-04-03 11:33:38 +0600

    change pause screen

--- scripts/MenuScripts/PauseScreen.gd
+++ scripts/MenuScripts/PauseScreen.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CanvasLayer
 
 func _ready():
 	# Убеждаемся, что сцена паузы обрабатывается всегда
@@ -8,9 +8,9 @@ func _on_ReturnToGameButton_pressed():
 	get_tree().paused = false
 	queue_free()
 
-func _on_ReturnToSettingsButton_pressed():
+func _on_RestartButton_pressed():
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/MenuScenes/Settings.tscn")
+	get_tree().reload_current_scene()
 
 func _on_ReturnToMenuButton_pressed():
 	get_tree().paused = false
